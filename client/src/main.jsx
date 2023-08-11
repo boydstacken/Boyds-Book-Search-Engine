532476c33b80bf82f291bbcd46f4c4c010a3bf08
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-11 10:42:29 -0500

    removed .js from client on the ReactDOM line

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client.js'
+import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
