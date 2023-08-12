ebce39697aed5c70bb4cefd60f7ee9596424437b
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-11 23:08:03 -0500

    fixed auth file and server file to properly execute graphql and alter Middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 
@@ -18,10 +18,20 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-app.use("/graphql", expressMiddleware(server));
+app.use("/graphql", expressMiddleware(server, {
+  context: authMiddleware
+}));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 db.once("open", () => {
   app.listen(PORT, () => {
