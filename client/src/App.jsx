faa0f7289ff8f090799b704bd0bb13d86ed816cb
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-13 12:29:01 -0500

    adjusted app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { LOGIN_USER, ADD_USER, SAVE_BOOK, REMOVE_BOOK } from './mutations'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, InMemoryCache } from 'apollo/client'
 
 function App() {
   return (
